bc181b68d8a16b8b0446d1d208a2d6d50bc9d03f
Krishanu Chakraborty <shanu.16k@gmail.com>
2022-10-08 13:06:42 +0530

    Add files via upload

---
+++ newsportal/includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; NewPortal <?php echo date('Y');?></p>
+      </div>
+      <!-- /.container -->
+    </footer>
---
+++ newsportal/includes/sidebar.php
@@ -0,0 +1,81 @@
+  <div class="col-md-4">
+
+          <!-- Search Widget -->
+          <div class="card mb-4">
+            <h5 class="card-header">Search</h5>
+            <div class="card-body">
+                   <form name="search" action="search.php" method="post">
+              <div class="input-group">
+           
+        <input type="text" name="searchtitle" class="form-control" placeholder="Search for..." required>
+                <span class="input-group-btn">
+                  <button class="btn btn-secondary" type="submit">Go!</button>
+                </span>
+              </form>
+              </div>
+            </div>
+          </div>
+
+          <!-- Categories Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Categories</h5>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+<?php $query=mysqli_query($con,"select id,CategoryName from tblcategory");
+while($row=mysqli_fetch_array($query))
+{
+?>
+
+                    <li>
+                      <a href="category.php?catid=<?php echo htmlentities($row['id'])?>"><?php echo htmlentities($row['CategoryName']);?></a>
+                    </li>
+<?php } ?>
+                  </ul>
+                </div>
+       
+              </div>
+            </div>
+          </div>
+
+          <!-- Side Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Recent News</h5>
+            <div class="card-body">
+                      <ul class="mb-0">
+<?php
+$query=mysqli_query($con,"select tblposts.id as pid,tblposts.PostTitle as posttitle from tblposts left join tblcategory on tblcategory.id=tblposts.CategoryId left join  tblsubcategory on  tblsubcategory.SubCategoryId=tblposts.SubCategoryId limit 8");
+while ($row=mysqli_fetch_array($query)) {
+
+?>
+
+  <li>
+                      <a href="news-details.php?nid=<?php echo htmlentities($row['pid'])?>"><?php echo htmlentities($row['posttitle']);?></a>
+                    </li>
+            <?php } ?>
+          </ul>
+            </div>
+          </div>
+
+
+   <!-- Side Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Popular  News</h5>
+            <div class="card-body">
+                      <ul >
+<?php
+$query1=mysqli_query($con,"select tblposts.id as pid,tblposts.PostTitle as posttitle from tblposts left join tblcategory on tblcategory.id=tblposts.CategoryId left join  tblsubcategory on  tblsubcategory.SubCategoryId=tblposts.SubCategoryId  order by viewCounter desc limit 5");
+while ($result=mysqli_fetch_array($query1)) {
+
+?>
+
+  <li>
+                      <a href="news-details.php?nid=<?php echo htmlentities($result['pid'])?>"><?php echo htmlentities($result['posttitle']);?></a>
+                    </li>
+            <?php } ?>
+          </ul>
+            </div>
+          </div>
+
+        </div>
